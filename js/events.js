cffce08215057fa4b92d5e83b2269212c67d5285
Andy Benedict <Andy.Benedict@cenveo.com>
2015-06-21 17:40:37 -0400

    Add clone repo keyframes

--- js/events.js
+++ js/events.js
@@ -186,7 +186,7 @@ states = [
         {"object":".ph1-2","time":0,"toggle":"unchanged"},
         {"object":".ph2-3","time":0,"toggle":"unchanged"},
         {"object":".master .pip","time":0,"toggle":"pip-q-h pip-4-s"},
-        {"pause":""},
+//        {"pause":""},
         
         {"object":".c-9","time":0,"toggle":"position1-2"},
         {"object":".line1-1-to-1-2","time":25,"toggle":"hidden"},
@@ -207,6 +207,105 @@ states = [
         {"object":".h-q","time":100,"toggle":"position1-1"},
         {"object":".line1-1-to-1-2","time":100,"toggle":"hidden"},
         
+//        {"pause":""},
+        {"object":".s-4","time":0,"toggle":"hidden"},
+        {"object":".h-q","time":0,"toggle":"hidden"},
+        {"object":".line1-1-to-1-2","time":0,"toggle":"hidden"},
+        {"object":".master","time":0,"toggle":"hidden"},
+        {"object":"body","time":0,"toggle":"klee"},
+        
+        /*
+         * Kaylee clones the repo
+         */
+        
+        /* Klee requests the repo */
+//        {"pause":""},
+        {"object":".s-4","time":0,"toggle":"hidden"},
+        {"object":".h-q","time":0,"toggle":"hidden"},
+        {"object":".line1-1-to-1-2","time":0,"toggle":"hidden"},
+        {"object":".master","time":0,"toggle":"hidden"},
+        {"object":"body","time":0,"toggle":"klee"},
+
+        /* Master resolves what needs to be transmitted */
+        {"pause":""},
+        {"object":".s-4","time":100,"toggle":"position1-2"},
+        {"object":".c-8","time":100,"toggle":"position1-2"},
+        {"object":".line1-2-to-1-3","time":0,"toggle":"hidden"},
+        {"object":".line1-2-to-2-3","time":0,"toggle":"hidden"},
+        {"object":".d-2","time":100,"toggle":"position1-3"},
+        {"object":".h-6","time":100,"toggle":"position2-3"},
+        {"object":".line2-3-to-2-4","time":100,"toggle":"hidden"},
+        {"object":".line2-3-to-3-4","time":0,"toggle":"hidden"},       
+        {"object":".s-9","time":100,"toggle":"position2-4"},
+        {"object":".c-7","time":100,"toggle":"position3-4"},
+        {"object":".line2-4-to-1-5","time":100,"toggle":"hidden"},
+        {"object":".line2-4-to-2-5","time":0,"toggle":"hidden"},
+        {"object":".d-j","time":100,"toggle":"position1-5"},
+        {"object":".c-5","time":100,"toggle":"position2-5"},       
+
+        {"pause":""},
+        {"object":".s-4","time":100,"toggle":"position1-1"},
+        {"object":".h-q","time":0,"toggle":"position1-1 position2-1"},
+        {"object":".c-9","time":100,"toggle":"position1-2"},
+        {"object":".c-8","time":0,"toggle":"position1-2 position2-1"},
+        {"object":".d-q","time":100,"toggle":"position2-3"},
+        {"object":".h-6","time":0,"toggle":"position2-3 position2-1"},
+        {"object":".d-5","time":100,"toggle":"position3-4"},
+        {"object":".c-7","time":0,"toggle":"position3-4 position2-1"},
+
+        {"pause":""},
+        {"object":".s-4","time":100,"toggle":"position1-1 position2-1"},
+        {"object":".c-9","time":100,"toggle":"position1-2 position2-1"},
+        {"object":".line1-1-to-1-2","time":0,"toggle":"hidden"},
+        {"object":".line1-2-to-1-3","time":0,"toggle":"hidden"},
+        {"object":".line1-2-to-2-3","time":0,"toggle":"hidden"},
+        {"object":".d-2","time":100,"toggle":"position1-3 position2-1"},
+        {"object":".d-q","time":100,"toggle":"position2-3 position2-1"},
+        {"object":".line2-3-to-2-4","time":100,"toggle":"hidden"},
+        {"object":".line2-3-to-3-4","time":0,"toggle":"hidden"},       
+        {"object":".s-9","time":100,"toggle":"position2-4 position2-1"},
+        {"object":".d-5","time":100,"toggle":"position3-4 position2-1"},
+        {"object":".line2-4-to-1-5","time":100,"toggle":"hidden"},
+        {"object":".line2-4-to-2-5","time":0,"toggle":"hidden"},
+        {"object":".d-j","time":100,"toggle":"position1-5 position2-1"},
+        {"object":".c-5","time":100,"toggle":"position2-5 position2-1"},
+
+        /* transfer files */
+        {"pause":""},
+        {"object":"body","time":0,"toggle":"klee"},
+        {"object":".master","time":0,"toggle":"hidden"},
+
+        {"pause":""},
+        {"object":".d-q","time":100,"toggle":"position2-1"},
+        {"object":".d-j","time":100,"toggle":"position2-1"},
+        {"object":".d-5","time":100,"toggle":"position2-1"},
+        {"object":".d-2","time":100,"toggle":"position2-1"},
+        {"object":".c-9","time":100,"toggle":"position2-1"},
+        {"object":".c-8","time":100,"toggle":"position2-1"},
+        {"object":".c-7","time":100,"toggle":"position2-1"},
+        {"object":".c-5","time":100,"toggle":"position2-1"},
+        {"object":".h-q","time":100,"toggle":"position2-1"},
+        {"object":".h-6","time":100,"toggle":"position2-1"},
+        {"object":".s-9","time":100,"toggle":"position2-1"},
+        {"object":".s-4","time":100,"toggle":"position2-1"},
+
+        /* Create master branch */
+        {"pause":""},
+        {"object":".master","time":0,"toggle":"hidden"},
+
+        /*
+         * Introduce origin/master
+         */
+        {"pause":""},
+        {"object":".s-a","time":0,"toggle":"nopip offscreen position4-4"},
+        
+        {"pause":""},        
+        {"object":".s-a .pip","time":0,"toggle":"pip-4-s"},
+
+        {"pause":""},
+        {"object":"body","time":0,"toggle":"klee"},
+        {"object":".s-a","time":0,"toggle":"hidden"},
+    
 /*	{"pause":""},
         {"object":".h-q","time":0,"toggle":"front center position1-1"},
         {"object":".ph1-1","time":200,"toggle":"unchanged"},
